bb4a3582fdb3083f16c6eb30300a27d5760f9401
Kamil Janku <k.janku1@gmail.com>
2021-07-04 09:21:15 +0200

    Refactor code to OOP

--- main.py
+++ main.py
@@ -12,50 +12,53 @@ auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 
 
-def get_tweets(keyword: str) -> List[str]:
-    all_tweets = []
-    for tweet in tweepy.Cursor(api.search, q=keyword, tweet_mode='extended', lang='pl').items(10):
-        all_tweets.append(tweet.full_text)
+class TweetProvider:
+    def get_tweets(keyword: str) -> List[str]:
+        all_tweets = []
+        for tweet in tweepy.Cursor(api.search, q=keyword, tweet_mode='extended', lang='pl').items(10):
+            all_tweets.append(tweet.full_text)
 
-    return all_tweets
+        return all_tweets
 
+    def clean_tweets(all_tweets: List[str]) -> List[str]:
+        tweets_clean = []
+        for tweet in all_tweets:
+            tweets_clean.append(p.clean(tweet))
 
-def clean_tweets(all_tweets: List[str]) -> List[str]:
-    tweets_clean = []
-    for tweet in all_tweets:
-        tweets_clean.append(p.clean(tweet))
-
-    return tweets_clean
-
-
-def get_sentiment(all_tweets: List[str]) -> List[float]:
-    sentiment_scores = []
-    for tweet in all_tweets:
-        blob = TextBlob(tweet)
-        sentiment_scores.append(blob.sentiment.polarity)
-
-    return sentiment_scores
+        return tweets_clean
 
 
-def get_avg_sentiment_score(keyword: str) -> int:
-    tweets = get_tweets(keyword)
-    tweets_clean = clean_tweets(tweets)
-    sentiment_scores = get_sentiment(tweets_clean)
+class Preference:
+    def get_preference_score(all_tweets: List[str]) -> List[float]:
+        preference_scores = []
+        for tweet in all_tweets:
+            blob = TextBlob(tweet)
+            preference_scores.append(blob.preference.polarity)
 
-    average_score = statistics.mean(sentiment_scores)
-    print(average_score)
-    return average_score
+        return preference_scores
+
+    def get_avg_preference_score(self, keyword: str) -> int:
+        tp = TweetProvider
+        tweets = tp.get_tweets(keyword)
+        tweets_clean = tp.clean_tweets(tweets)
+        preference_scores = self.get_preference_score(tweets_clean)
+
+        average_score = statistics.mean(preference_scores)
+        print(average_score)
+        return average_score
 
 
 if __name__ == "__main__":
+
+    pref = Preference
     print("Co lepiej zrobić?")
     first_thing = input()
     print('...czy...')
     second_thing = input()
     print("\n")
 
-    first_score = get_avg_sentiment_score(first_thing)
-    second_score = get_avg_sentiment_score(second_thing)
+    first_score = pref.get_avg_preference_score(first_thing)
+    second_score = pref.get_avg_preference_score(second_thing)
 
     if first_score == second_score:
         print("bez znaczenia")
